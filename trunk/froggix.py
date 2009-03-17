71c4c8f6603d59e3a5e68f8245b9dc49c6ca101d
scifly <scifly@f2f7e45f-0d87-4489-93c6-6f8f22487e1d>
2009-03-17 14:49:05 +0000

    Begun to refactor the code into a more modular OOP style.  Initial move of classes into seperate modules and out of froggix.py. Modified the GPL header so it takes account of the multiple files. Begun to use Eclipse, so have set ignore on the project files.

--- trunk/froggix.py
+++ trunk/froggix.py
@@ -1,16 +1,21 @@
 """
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Copyright 2009 Paul Elms
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+This file is part of Froggix.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Froggix is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Froggix is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Froggix.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 
 
@@ -21,88 +26,9 @@ import platform
 import cPickle
 import operator
 
-#Classes used in the Game are defined here
+from Moveable import *
+from Home import Home
 
-class Sprite:
-    def __init__(self,x_pos,y_pos,filename, alt_filename):
-        self.x, self.y =  x_pos, y_pos
-        self.bitmap = image.load(filename)
-        if alt_filename:
-            self.alt_bitmap = image.load(alt_filename)
-        else:
-            self.alt_bitmap = None
-        self.draw = True
-    def render(self):
-        if self.draw:
-            screen.blit(self.bitmap, (self.x,self.y))
-
-
-class Moveable(Sprite):
-    def __init__(self,x_pos,y_pos,filename,alt_filename, speed):
-        Sprite.__init__(self,x_pos,y_pos,filename, alt_filename)
-        self.speed = speed
-        self.width = self.bitmap.get_width()
-        self.height = self.bitmap.get_height()
-    def move(self,  level):
-        if self.x < (0 - self.width): # Moved off left edge of screen.
-            self.x = 640
-        elif self.x > 640:  # Moved off right edge of screen.
-            self.x = (0 - self.width)
-        else: 
-         self.x -= self.speed + (level * 0.5 * self.speed)
-
-class TimedMoveable(Moveable):
-    def __init__(self, x_pos, y_pos, filename, alt_filename, speed, delay, locs):
-        Moveable.__init__(self, x_pos, y_pos, filename, alt_filename, speed)
-        if delay:
-            self.delay = delay * 1000  #delay is in seconds, while self.delay needs to be msecs
-            self.timer = time.get_ticks()
-        self.locations = locs
-    def move(self,  level):
-        #determine if a change in location is needed (level is not used at present)
-        if (self.timer + self.delay) < time.get_ticks():
-         new_loc = random.choice(self.locations)
-         self.x = new_loc[0]
-         self.y = new_loc[1] 
-         self.timer = time.get_ticks()
-        
-        #TODO add ability of crocs and flies to be on other moveables.
-
-class RiverMoveable(TimedMoveable):
-    def __init__(self, x_pos, y_pos, filename, alt_filename, speed, delay = None, locs = None):
-        TimedMoveable.__init__(self, x_pos, y_pos, filename, alt_filename, speed, delay, locs)
-        if delay:
-            self.sinkable = True
-        else:
-            self.sinkable = False
-        self.temp_bitmap = None
-        self.timer = None
-        self.sinking = False
-    def move(self,  level):
-        Moveable.move(self,  level)  #Recall the move function from Moveable
-        if self.sinking == True and (self.timer + 4*self.delay) < time.get_ticks():#Have sunk so need to surface
-            self.timer = None
-            self.bitmap = self.temp_bitmap
-            self.temp_bitmap = None
-            self.sinking = False
-            self.draw = True
-    def sink(self):
-        if self.timer == None:
-            self.timer = time.get_ticks()
-        if self.temp_bitmap == None:  # ensures this only happens once
-            self.temp_bitmap = self.bitmap
-            self.sinking = True
-        if (self.timer + self.delay) < time.get_ticks(): #start to sink
-            if self.bitmap != self.alt_bitmap:  
-                self.bitmap = self.alt_bitmap
-        if (self.timer + 2*self.delay) < time.get_ticks(): #Sunk
-            self.draw = False
-        
-
-class Home:
-    def __init__(self, x_pos,  y_pos):
-        self.x,  self.y = x_pos, y_pos
-        self.width,  self.height = 45,  45
 
 #Global constants
 game_length = 60000.0  # 60 seconds
@@ -492,7 +418,7 @@ while quit == 0:
         #Draw all objects.  The order is important. frogs need to be on top of all but vehicles
         to_draw = [ 'river_objects', 'flies', 'crocs', 'frogs',  'vehicles']
         for li in to_draw:
-            [obj.render() for obj in game_data[li]]
+            [obj.render(screen) for obj in game_data[li]]
         
         #Draw score to screen
         scoretext = scorefont.render("Score: " + str(score), True, (220, 0, 0))
